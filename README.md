5bdac66df439efddf050b190f980084a98336446
Swall0w <technext.jpn@gmail.com>
2017-02-16 12:27:43 +0900

    modified

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # word2vec_demo
+This is a simple word2vec program using gensim.
+It has a wakati code to separate japanese sentenences to sequence of japanese words, a train code to train word2vec model, a test mode that estimates some similar words.
--- similars.py
+++ similars.py
@@ -4,10 +4,7 @@ from gensim.models import word2vec
 import sys
 
 model = word2vec.Word2Vec.load(sys.argv[1])
-#results = model.most_similar(positive=sys.argv[2],topn=10)
 results = model.most_similar(positive=sys.argv[2],topn=10)
 
 for result in results:
     print(result[0],'\t', result[1])
-#print(model[sys.argv[1]])
-#print(len(model[sys.argv[1]]))
